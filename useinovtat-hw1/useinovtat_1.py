85551b40cd52050ee155d213f0865f3787654d65
EsferUs7 <150047773+EsferUs7@users.noreply.github.com>
2023-11-08 18:05:25 +0200

    Delete useinovtat-hw1 directory
    :)

--- useinovtat-hw1/useinovtat_1.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    suma = 0
-    if n < 4:
-        return 0
-    else:
-        for i in range(3, n):
-            if i % 3 == 0 or i % 5 == 0:
-                suma += i
-        return suma
--- useinovtat-hw1/useinovtat_2.py
+++
@@ -1,5 +0,0 @@
-def hanoi(tower):
-    if tower == 1:
-        return 1
-    else:
-        return 2*hanoi(tower-1) + 1
